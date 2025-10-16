22e9923dd2b5b7ff0cadac31188327e305e61f0c
arpiringProgrammer <149250712+arpiringProgrammer@users.noreply.github.com>
2025-10-16 10:52:34 -0300

    Add files via upload

--- headerFileCpp.h
+++ headerFileCpp.h
@@ -4,36 +4,19 @@
 #include <string>
 using namespace std;
 
-
-
-/* Representa um objeto imutável com o valor de um número inteiro positivo usando os algarismos arábicos e
-romanos.
-*/
-class NumeroRomano {
+class Hexadecimal {
 private:
-    string numeroRomano;
-    unsigned int numeroArabico;
+    string hexadecimal;
+
 public:
-// Inicia os atributos do objeto NumeroRomano com os valores do número arábico e romano.
-NumeroRomano(unsigned int numeroArabico);
-// Inicia os atributos do objeto NumeroRomano com os valores do número romano e arábico.
-NumeroRomano(string numeroRomano);
-// Retorna o valor do número romano.
-string getNumeroRomano();
-// Retorna o valor do número arábico.
-unsigned int getNumeroArabico();
-/* Retorna uma string usando o formato NúmeroArábico -> NúmeroRomano
-Exemplos:
+    Hexadecimal() = default;
+    Hexadecimal(string hexadecimal);
 
-27 -> XXVII
-150 -> CL
+    void setNumber(string hexadecimal);
+    string getHexadecimal(); // ← nome alterado
+    unsigned int decimal();
 
-*/
-string toString();
-// Converte o número arábico para romano.
-string converterArabicoParaRomano(unsigned int numeroArabico);
-// Converte o número romano para arábico.
-unsigned int converterRomanoParaArabico(string numeroRomano);
+    operator int(); // permite retorno direto no main
 };
 
 #endif
--- practiceFile.cpp
+++ practiceFile.cpp
@@ -1,130 +1,46 @@
-#include <string>
 #include <iostream>
+#include <cmath>
 #include "headerFileCpp.h"
 
 using namespace std;
 
-
-// Inicia os atributos do objeto NumeroRomano com os valores do número arábico e romano.
-NumeroRomano::NumeroRomano(unsigned int numeroArabico) : numeroArabico(numeroArabico) {}
-// Inicia os atributos do objeto NumeroRomano com os valores do número romano e arábico.
-NumeroRomano::NumeroRomano(string numeroRomano) : numeroRomano(numeroRomano) {}
-// Retorna o valor do número romano.
-string NumeroRomano::getNumeroRomano() {
-    return numeroRomano;
+Hexadecimal::Hexadecimal(string hexadecimal) {
+    this->hexadecimal = hexadecimal;
 }
-// Retorna o valor do número arábico.
-unsigned int NumeroRomano::getNumeroArabico() {
-    return numeroArabico;
-}
-/* Retorna uma string usando o formato NúmeroArábico -> NúmeroRomano
-Exemplos:
 
-27 -> XXVII
-150 -> CL
-
-*/
-string NumeroRomano::toString() {
-    string roman = converterArabicoParaRomano(numeroArabico);
-    return to_string(numeroArabico) + "-> " + roman;
+void Hexadecimal::setNumber(string hexadecimal) {
+    this->hexadecimal = hexadecimal;
 }
-// Converte o número arábico para romano.
-string NumeroRomano::converterArabicoParaRomano(unsigned int numeroArabico) {
-    string romanNumber;
-    while(numeroArabico > 0) {
-        if(numeroArabico >= 1000) {
-            romanNumber += 'M';
-            numeroArabico -= 1000;
-        } else if(numeroArabico >= 900) {
-            romanNumber += "CM";
-            numeroArabico -= 900;
-        } else if(numeroArabico >= 500) {
-            romanNumber += 'D';
-            numeroArabico -= 500;
-        } else if(numeroArabico >= 400) {
-            romanNumber += "CD";
-            numeroArabico -= 400;
-        } else if(numeroArabico >= 100) {
-            romanNumber += 'C';
-            numeroArabico -= 100;
-        } else if(numeroArabico >= 90) {
-            romanNumber += "XC";
-            numeroArabico -= 90;
-        } else if(numeroArabico >= 50) {
-            romanNumber += 'L';
-            numeroArabico -= 50;
-        } else if(numeroArabico >= 40) {
-            romanNumber += "XL";
-            numeroArabico -= 40;
-        } else if(numeroArabico >= 10) {
-            romanNumber += 'X';
-            numeroArabico -= 10;
-        } else if(numeroArabico == 9) {
-            romanNumber += "IX";
-            numeroArabico -= 9;
-        } else if(numeroArabico >= 5) {
-            romanNumber += 'V';
-            numeroArabico -= 5;
-        } else if(numeroArabico == 4) {
-            romanNumber += "IV";
-            numeroArabico -= 4;
-        }else if(numeroArabico >= 1) {
-            romanNumber += 'I';
-            numeroArabico -= 1;
+
+string Hexadecimal::getHexadecimal() {
+    return this->hexadecimal;
+}
+
+unsigned int Hexadecimal::decimal() {
+    int number = 0;
+    int counter = 0;
+
+    for (int index = hexadecimal.length() - 1; index >= 0; index--, counter++) {
+        char hexa = hexadecimal[index];
+        if (hexa >= '0' && hexa <= '9') {
+            number += (hexa - '0') * pow(16, counter);
+        } else if (hexa >= 'A' && hexa <= 'F') {
+            number += (hexa - 'A' + 10) * pow(16, counter);
+        } else if (hexa >= 'a' && hexa <= 'f') {
+            number += (hexa - 'a' + 10) * pow(16, counter);
         }
     }
-    return romanNumber;
-}
-// Converte o número romano para arábico.
-unsigned int NumeroRomano::converterRomanoParaArabico(string numeroRomano) {
-    int number = 0, index = 0;
-    while(numeroRomano[index] != '\0') {
-        if(numeroRomano[index] == 'M') {
-            number += 1000;
-        }
-        else if(numeroRomano[index] == 'C') {
-            if((index + 1 < numeroRomano.length()) && numeroRomano[index + 1] == 'D') {
-                number += 400;
-                index++;
-            } else if((index + 1 < numeroRomano.length()) && numeroRomano[index + 1] == 'M') {
-                number += 900;
-                index++;
-            } else {
-                number += 100;
-            }
-        }
-        else if(numeroRomano[index] == 'D') {
-            number += 500;
-        }
-        else if(numeroRomano[index] == 'X') {
-            if((index + 1 < numeroRomano.length()) && numeroRomano[index + 1] == 'L') {
-                number += 40;
-                index++;
-            } else if((index + 1 < numeroRomano.length()) && numeroRomano[index + 1] == 'C') {
-                number += 90;
-                index++;
-            } else {
-                number += 10;
-            }
-        }
-        else if(numeroRomano[index] == 'L') {
-            number += 50;
-        }
-        else if(numeroRomano[index] == 'I') {
-            if((index + 1 < numeroRomano.length()) && numeroRomano[index + 1] == 'X') {
-                number += 9;
-                index++;
-            } else if((index + 1 < numeroRomano.length()) && numeroRomano[index + 1] == 'V') {
-                number += 4;
-                index++;
-            } else {
-                number += 1;
-            }
-        }
-        else if(numeroRomano[index] == 'V') {
-            number += 5;
-        }
-        index++; 
-    }
+
     return number;
+}
+
+Hexadecimal::operator int() {
+    setNumber("f");
+    cout << "The hexadecimal of the number " << getHexadecimal()
+         << " is " << decimal() << endl;
+    return 0;
+}
+
+int main() {
+    return Hexadecimal();
 }
